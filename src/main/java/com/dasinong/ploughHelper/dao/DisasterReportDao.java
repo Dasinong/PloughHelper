8bb476228c96a173833eb557920e25fe6b1d14f7
sunzc <1017665057@qq.com>
2015-07-03 11:45:48 +0800

    add DisasterReportDao.java

--- src/main/java/com/dasinong/ploughHelper/dao/DisasterReportDao.java
+++ src/main/java/com/dasinong/ploughHelper/dao/DisasterReportDao.java
@@ -1,9 +1,52 @@
+/**
+ * 
+ */
 package com.dasinong.ploughHelper.dao;
 
-public class DisasterReportDao {
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-	public DisasterReportDao() {
-		// TODO Auto-generated constructor stub
+import com.dasinong.ploughHelper.model.DisasterReport;
+
+/**
+ * @author Dell
+ *
+ */
+public class DisasterReportDao extends HibernateDaoSupport implements
+		IDisasterReportDao {
+
+	/* (non-Javadoc)
+	 * @see com.dasinong.ploughHelper.dao.IDisasterReport#save(com.dasinong.ploughHelper.model.DisasterReport)
+	 */
+	@Override
+	public void save(DisasterReport disasterReport) {
+		getHibernateTemplate().save(disasterReport);		
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dasinong.ploughHelper.dao.IDisasterReport#update(com.dasinong.ploughHelper.model.DisasterReport)
+	 */
+	@Override
+	public void update(DisasterReport disasterReport) {
+		getHibernateTemplate().update(disasterReport);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dasinong.ploughHelper.dao.IDisasterReport#delete(com.dasinong.ploughHelper.model.DisasterReport)
+	 */
+	@Override
+	public void delete(DisasterReport disasterReport) {
+		getHibernateTemplate().delete(disasterReport);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.dasinong.ploughHelper.dao.IDisasterReport#findByDisasterName(java.lang.String)
+	 */
+	@Override
+	public DisasterReport findByDisasterName(String disasterName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
