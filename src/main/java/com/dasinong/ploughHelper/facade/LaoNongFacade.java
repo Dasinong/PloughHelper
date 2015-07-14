1e35764ba7d2199b06c45a96b6af61151fbd738f
sunzc <1017665057@qq.com>
2015-07-14 15:32:32 +0800

    add Agriculture Disaster Report in LaoNong Facade

--- src/main/java/com/dasinong/ploughHelper/facade/LaoNongFacade.java
+++ src/main/java/com/dasinong/ploughHelper/facade/LaoNongFacade.java
@@ -6,8 +6,11 @@ import java.util.Random;
 import com.dasinong.ploughHelper.modelTran.LaoNong;
 import com.dasinong.ploughHelper.modelTran.NongYan;
 import com.dasinong.ploughHelper.modelTran.WeatherAlert;
+import com.dasinong.ploughHelper.weather.All24h;
 import com.dasinong.ploughHelper.weather.AllLocation;
 import com.dasinong.ploughHelper.weather.GetWeatherAlert;
+import com.dasinong.ploughHelper.weather.AgriDisForcast;
+import com.dasinong.ploughHelper.weather.AllAgriDisForcast;
 
 public class LaoNongFacade implements ILaoNongFacade {
 
@@ -36,10 +39,24 @@ public class LaoNongFacade implements ILaoNongFacade {
 		HashMap<String,Object> result = new HashMap<String,Object>();
 		GetWeatherAlert gwa = new GetWeatherAlert(areaId.toString()); 
 		WeatherAlert wa = gwa.getWeatherAlert();
+		String DisasterInfo = "";
+		String AgriURLTag = "";
+		try{
+			 DisasterInfo = AllAgriDisForcast.getadf().getadf(areaId).getDisasterInfo();
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Error happend when get Agriculture Disaster Forcast");
+		}
 		if (wa!=null){
 			result.put("respCode", 200);
 			result.put("message","获得老农成功");
 			LaoNong laoNong = new LaoNong(0,2,"ohnoface.png","天气预警",wa.shortDescription(),wa.urlTag());
+			result.put("data",laoNong);
+			return result;
+		} else if(DisasterInfo != ""){
+			result.put("respCode", 200);
+			result.put("message","获得老农成功");
+			LaoNong laoNong = new LaoNong(0,2,"ohnoface.png","农业预警",DisasterInfo, AgriURLTag);
 			result.put("data",laoNong);
 			return result;
 		}
