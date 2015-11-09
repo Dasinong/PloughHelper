31ef735e3953896f50bc2660eb6ad6caa40efef9
Xiahong gao <xiahonggao@XiahongdeMacBook-Pro.local>
2015-11-10 01:29:26 +0800

    Add more data to error response
    Summary:
    前端可以用这些数据
    1. log到友盟
    2. 更好的错误UI
    
    Test Plan: test in production
    
    Reviewers: gaoxh04
    
    Reviewed By: gaoxh04
    
    Differential Revision: http://phabricator.dasinong666.com/D53

--- src/main/java/com/dasinong/ploughHelper/BaseController.java
+++ src/main/java/com/dasinong/ploughHelper/BaseController.java
@@ -117,8 +117,11 @@ public class BaseController {
 		InvalidUserAccessTokenException exception
 	) {
 		Map<String,Object> result = new HashMap<String,Object>();
+		Map<String,Object> errorData = new HashMap<String,Object>();
+		errorData.put("accessToken", exception.getToken());
 		result.put("respCode", 120);
 		result.put("message", "不合法的User Access Token");
+		result.put("data", errorData);
 		return result;
 	}
 	
@@ -158,13 +161,12 @@ public class BaseController {
 		HttpServletRequest req, 
 		MissingParameterException exception
 	) {
-		Map<String,Object> result = new HashMap<String,Object>();
+		Map<String, Object> result = new HashMap<String, Object>();
+		Map<String, Object> errorData = new HashMap<String, Object>();
+		errorData.put("name", exception.getParamName());
 		result.put("respCode", 300);
-		if (exception.getParamName() != null) {
-			result.put("message", exception.getParamName() + "参数缺失");
-		} else {
-			result.put("message", "参数不全");
-		}
+		result.put("message", "参数缺失");
+		result.put("data", errorData);
 		return result;
 	}
 	
@@ -176,8 +178,11 @@ public class BaseController {
 		InvalidParameterException exception
 	) {
 		Map<String,Object> result = new HashMap<String,Object>();
-		result.put("respCode", 300);
-		result.put("message", exception.getParamName() + "参数不正确");
+		Map<String, Object> errorData = new HashMap<String, Object>();
+		errorData.put("name", exception.getParamName());
+		result.put("respCode", 301);
+		result.put("message", "参数不正确");
+		result.put("data", errorData);
 		return result;
 	}
 	
@@ -193,8 +198,12 @@ public class BaseController {
 		ResourceNotFoundException exception
 	) {
 		Map<String,Object> result = new HashMap<String,Object>();
+		Map<String,Object> errorData = new HashMap<String,Object>();
+		errorData.put("resourceType", exception.getResourceType());
+		errorData.put("resourceId", exception.getResourceId());
 		result.put("respCode", 404);
-		result.put("message", "无法找到"+exception.getResourceType()+exception.getResourceId());
+		result.put("message", "请求的资源不存在");
+		result.put("data", errorData);
 		return result;
 	}
 	
@@ -209,8 +218,12 @@ public class BaseController {
 		DataAccessException exception
 	) {
 		Map<String,Object> result = new HashMap<String,Object>();
+		Map<String,Object> errorData = new HashMap<String,Object>();
+		errorData.put("class", exception.getClass());
+		errorData.put("stacktrace", exception.getStackTrace());
 		result.put("respCode", 1000);
 		result.put("message", "数据库访问错误");
+		result.put("data", errorData);
 		return result;
 	}
 		
@@ -236,8 +249,12 @@ public class BaseController {
 	@ResponseBody
 	public Object handleError(HttpServletRequest req, Exception exception) {
 		Map<String,Object> result = new HashMap<String,Object>();
+		Map<String,Object> errorData = new HashMap<String,Object>();
+		errorData.put("class", exception.getClass());
+		errorData.put("stacktrace", exception.getStackTrace());
 		result.put("respCode", 500);
 		result.put("message", "服务器内部错误");
+		result.put("data", errorData);
 		return result;
 	}
 	
